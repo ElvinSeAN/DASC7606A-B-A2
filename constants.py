6de37e940a40c63a51881fbbcbfac1a004e2cf80
ElvinSeAN <uron799@gmail.com>
2025-11-13 23:33:11 +0800

    Change model checkpoint from Helsinki-NLP to facebook/bart-large

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 OUTPUT_DIR = "./results"
 MAX_INPUT_LENGTH = 128
 MAX_TARGET_LENGTH = 128
-MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-zh-en"
+# MODEL_CHECKPOINT = "Helsinki-NLP/opus-mt-zh-en"
+MODEL_CHECKPOINT = "facebook/bart-large"
